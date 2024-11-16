1f36c394d0899f1628e0074e1031fcf14ac11667
DanielKremes <danielkremes06@gmail.com>
2024-11-16 20:28:38 -0300

    Create project

---
+++ src/student/Student.java
@@ -0,0 +1,87 @@
+package student;
+
+public class Student {
+
+    // Attributes
+    private String name;
+    private int age;
+    private String birthDate;
+    private String numberCPF;
+    private String rg;
+    private String fatherName;
+    private String motherName;
+    private String gradeEnrollment;
+    private String nameSchool;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNumberCPF() {
+        return numberCPF;
+    }
+
+    public void setNumberCPF(String numberCPF) {
+        this.numberCPF = numberCPF;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getMotherName() {
+        return motherName;
+    }
+
+    public void setMotherName(String motherName) {
+        this.motherName = motherName;
+    }
+
+    public String getGradeEnrollment() {
+        return gradeEnrollment;
+    }
+
+    public void setGradeEnrollment(String gradeEnrollment) {
+        this.gradeEnrollment = gradeEnrollment;
+    }
+
+    public String getNameSchool() {
+        return nameSchool;
+    }
+
+    public void setNameSchool(String nameSchool) {
+        this.nameSchool = nameSchool;
+    }
+}
